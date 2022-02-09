eac19740aed4ba74975cd0a7db51fb12972db849
alexsorin753 <alex.songureanu74@gmail.com>
2022-02-09 08:57:44 +0200

    fixed sidebar done, side navigation started

---
+++ public/javascripts/external_pages.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+
+};
+
+// use it if you make changes to the DOM on load
+document.addEventListener('DOMContentLoaded', function() {
+   console.log('localhost')
+});
--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,63 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* GET button page. */
+// GET button page.
 router.get('/buttons', function(req, res, next) {
   res.render('buttons', { title: 'Buttons' });
 });
 
-/* GET images page. */
+// GET images page.
 router.get('/images', function(req, res, next) {
   res.render('images', { title: 'Images' });
 });
 
-/* GET menu page. */
+// GET menu page.
 router.get('/menu', function(req, res, next) {
   res.render('menu', { title: 'Menu' });
 });
 
-/* GET forms page. */
+// GET forms page.
 router.get('/forms', function(req, res, next) {
   res.render('forms', { title: 'Forms' });
 });
 
-/* GET filters page. */
+// GET filters page.
 router.get('/filters', function(req, res, next) {
   res.render('filters', { title: 'Filters' });
 });
 
-/* GET tables page. */
+// GET tables page.
 router.get('/tables', function(req, res, next) {
   res.render('tables', { title: 'Tables' });
 });
 
-/* GET more page. */
+// GET more page.
 router.get('/more', function(req, res, next) {
   res.render('more', { title: 'More' });
 });
 
-/* GET other page. */
+// GET other page.
 router.get('/other', function(req, res, next) {
   res.render('other', { title: 'Other' });
+});
+
+// GET fixed-sidebar-full page.
+router.get('/fixed-sidebar-full', function(req, res, next) {
+  res.render('external/fixed_sidebar_full', { title: 'Full Fixed Sidebar'});
+});
+
+// GET fixed-sidebar-auto page.
+router.get('/fixed-sidebar-auto', function(req, res, next) {
+  res.render('external/fixed_sidebar_auto', { title: 'Auto Fixed Sidebar'});
+});
+
+// GET side-navigation page.
+router.get('/side-navigation', function(req, res, next) {
+  res.render('external/side_navigation', { title: 'Side Navigation'});
 });
 module.exports = router;
